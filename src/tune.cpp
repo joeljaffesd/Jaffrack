07c2fbe9b5ec6120cc097c432ad877c327f7dd5b
joeljaffesd <joel@jaffesd.com>
2024-07-05 18:56:30 +0100

    Added Tuning App

---
+++ src/tune.cpp
@@ -0,0 +1,50 @@
+#include "audioTemplate.hpp"
+
+#include "../Gimmel/include/gimmel.hpp"
+
+template <typename T>
+struct tuningApp : audioTemplate<T> {
+  // add objects to app
+  giml::Timer<T> timer;
+  giml::SinOsc<T> osc;
+
+
+  // configure objects in constructor 
+  tuningApp(int sampleRate, int blockSize, int audioOutputs, int audioInputs) :
+  audioTemplate<T>(sampleRate, blockSize, audioOutputs, audioInputs), // <- call base class constructor 
+  // call effect constructors 
+  osc(sampleRate){}
+
+  // override processAudio to add fx to signal chain
+  T processAudio (T in) override {
+    timer.tick();
+    auto timeValue = timer.timeU();
+    if (timeValue < 0.25) {
+        osc.setFrequency(41.2);
+    } else if (timeValue < 0.5) {
+        osc.setFrequency(55);
+    } else if (timeValue < 0.75) {
+        osc.setFrequency(73.42);
+    } else {
+        osc.setFrequency(98);
+    }
+    return osc.processSample() * !timer.isDone();
+  }
+
+  std::string processLine(std::string in) override {
+    if (in == "tune") {
+      this->timer.set(this->sampleRate * 4);
+      return "Tuning";
+    } else {return "";}
+  }
+
+  std::string initMessage() override {
+    return "'tune' to tune. `Enter` to quit.";
+  }
+};
+
+int main() {
+  tuningApp<float> app(48000, 128, 2, 2);
+  app.start();
+  return 0;
+}
