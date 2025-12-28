5c191167af8c4874fa7c6f07652e7b16747f00b4
joeljaffesd <joel@jaffesd.com>
2025-12-28 18:35:49 +0000

    Prototyped audio param update on cuttleSend

--- src/cuttleSend.cpp
+++ src/cuttleSend.cpp
@@ -4,6 +4,8 @@
 #include "../al_ext/statedistribution/cuttlebone/Cuttlebone/Cuttlebone.hpp"
 using namespace cuttlebone;
 
+#include "al/ui/al_ParameterServer.hpp"
+
 #define MAIN
 #include "wetDryWet.cpp"
 
@@ -16,16 +18,34 @@ struct cuttleSend : wetDryWet<T> {
   const char* targetAddress;
   Maker<SharedState> maker;
   SharedState* localState = new SharedState;
+  float gain = 1.f;
+
+  ParameterBool mute {"mute", "", false};
+  al::ParameterServer parameterServer {"0.0.0.0", 9010};
 
   cuttleSend(int sampleRate, int blockSize, int audioOutputs, int audioInputs, const char* target = "127.0.0.1") :
   wetDryWet<T>(sampleRate, blockSize, audioOutputs, audioInputs), targetAddress(target), maker(target)
-  {maker.start();}
+  {
+    maker.start();
+    parameterServer.verbose(true);
+    parameterServer.registerParameter(mute);
+    mute.registerChangeCallback([this](float value) {
+      if (value <= 0.5f) {
+        gain = 0.f;
+      } 
+      else {
+        gain = 1.f;
+      }
+      std::cout << "Mute set to " << (value <= 0.5f ? "ON" : "OFF") << std::endl;
+    });
+  }
 
   void onSound(AudioIOData &io) override {
     wetDryWet<T>::onSound(io);
     for (int sample = 0; sample < this->blockSize; sample++) {
       float amp = 0.f;
       for (int channel = 0; channel < this->channelsOut; channel++){
+        io.out(channel, sample) *= gain;
         amp += io.out(channel, sample);
       }
       amp /= this->channelsOut;
