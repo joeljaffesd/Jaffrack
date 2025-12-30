e3efccecd3ff77c51cc47d2b55baf53746476422
Joel A. Jaffe <joel@jaffesd.com>
2025-12-30 09:11:41 -0800

    Knob: Encapsulated in `include/knob.hpp`

---
+++ include/knob.hpp
@@ -0,0 +1,103 @@
+#ifndef KNOB_HPP
+#define KNOB_HPP
+
+#include "al/graphics/al_Graphics.hpp"
+#include "al/graphics/al_Mesh.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/math/al_Functions.hpp"
+
+class Knob {
+private:
+  al::Vec2f center = al::Vec2f(0.f, 0.f);
+  float width = 1.f, height = 1.f, padding = 0.1f;
+  float scale = (width - padding) * (height - padding) / (width * height);
+
+  al::Mesh mOuterCircle;
+  al::Mesh mInnerCircle;
+  al::Mesh mIndicatorLine;
+
+  // float endAngle = 225 * M_PI / 180.0f;
+  float currentEndAngleDeg = -45.f;
+  const float startAngleDeg = -45.f;
+  const float maxEndAngleDeg = 225.f;
+
+  float currentParamValue = 0.f; // normalized [0,1] value
+
+public:
+  // methods
+  void seed() {
+    al::addCircle(mOuterCircle, 1.f, 64);
+    for (int i = 0; i < mOuterCircle.vertices().size(); i++) {
+      mOuterCircle.color(al::HSV(0, 0, 1)); // color
+    }
+    mOuterCircle.scale(scale);
+
+    al::addCircle(mInnerCircle, 0.5f, 64);
+    for (int i = 0; i < mInnerCircle.vertices().size(); i++) {
+      mInnerCircle.color(al::HSV(0, 0, 1)); // color
+    }
+    mInnerCircle.scale(scale);
+
+    updateIndicator(currentEndAngleDeg * M_PI / 180.f);
+  }
+
+  void updateIndicator(float newEndAngleInRadians) {
+    mIndicatorLine.reset();
+    mIndicatorLine.primitive(al::Mesh::TRIANGLE_STRIP);
+    float innerRadius = 0.5f;
+    float outerRadius = 1.0f;
+    float startAngle = startAngleDeg * M_PI / 180.f;
+    float end = newEndAngleInRadians;
+
+    // If end is behind start in radians, wrap it by adding 2*PI
+    if (end < startAngle) end += 2.0f * M_PI;
+
+    // add slices
+    constexpr int slices = 32;
+    // float step = (end - startAngle) / slices;
+
+    float step = (startAngle - end) / slices;
+
+    for (int i = 0; i <= slices; ++i) {
+      float theta = startAngle + i * step - (M_PI/2); 
+      mIndicatorLine.vertex(cos(theta) * innerRadius, sin(theta) * innerRadius);
+      mIndicatorLine.vertex(cos(theta) * outerRadius, sin(theta) * outerRadius);
+    }
+
+    // add color
+    for (size_t i = 0; i < mIndicatorLine.vertices().size(); ++i) {
+      mIndicatorLine.color(al::HSV(0.55, 0.5, 1)); // light blue
+    }
+
+    // scale
+    mIndicatorLine.scale(scale);
+  }
+
+  void printState() {
+    printf("Knob angle: %.2f degrees\n", currentEndAngleDeg);
+    printf("Knob param value: %.3f\n", currentParamValue);
+  }
+
+  void mouseEvent(al::Vec2f normalizedPos) {
+    al::Vec2f diff = normalizedPos - this->center;
+    float angle = atan2(diff[1], diff[0]) * 180.0f / M_PI;
+
+    // Normalize angle returned by atan2 into [startAngleDeg, startAngleDeg + 360)
+    if (angle < startAngleDeg) angle += 360.0f;
+
+    // Clamp to allowed range
+    angle = al::clip(angle, maxEndAngleDeg, startAngleDeg);
+    angle = startAngleDeg + (maxEndAngleDeg - angle);
+
+    this->currentEndAngleDeg = angle;
+    this->currentParamValue = al::mapRange(angle, -45.f, 225.f, 0.f, 1.f);
+    updateIndicator(this->currentEndAngleDeg * M_PI / 180.f);
+  }
+
+  void draw(al::Graphics& g) {
+    g.draw(mOuterCircle);
+    g.draw(mIndicatorLine);
+  }
+};
+
+#endif  // KNOB_HPP
--- src/knob.cpp
+++ src/knob.cpp
@@ -15,107 +15,11 @@
 #include "../allolib/include/al/math/al_Random.hpp"
 #include "../al_ext/statedistribution/cuttlebone/Cuttlebone/Cuttlebone.hpp"
 
-class Knob {
-private:
-  Vec2f center = Vec2f(0.f, 0.f);
-  float width = 1.f, height = 1.f, padding = 0.1f;
-  float scale = (width - padding) * (height - padding) / (width * height);
-
-  Mesh mOuterCircle;
-  Mesh mInnerCircle;
-  Mesh mIndicatorLine;
-
-  // float endAngle = 225 * M_PI / 180.0f;
-  float currentEndAngleDeg = -45.f;
-  const float startAngleDeg = -45.f;
-  const float maxEndAngleDeg = 225.f;
-
-  float currentParamValue = 0.f; // normalized [0,1] value
-
-public:
-  // methods
-  void seed() {
-    al::addCircle(mOuterCircle, 1.f, 64);
-    for (int i = 0; i < mOuterCircle.vertices().size(); i++) {
-      mOuterCircle.color(HSV(0, 0, 1)); // color
-    }
-    mOuterCircle.scale(scale);
-
-    al::addCircle(mInnerCircle, 0.5f, 64);
-    for (int i = 0; i < mInnerCircle.vertices().size(); i++) {
-      mInnerCircle.color(HSV(0, 0, 1)); // color
-    }
-    mInnerCircle.scale(scale);
-
-    updateIndicator(currentEndAngleDeg * M_PI / 180.f);
-  }
-
-  void updateIndicator(float newEndAngleInRadians) {
-    mIndicatorLine.reset();
-    mIndicatorLine.primitive(Mesh::TRIANGLE_STRIP);
-    float innerRadius = 0.5f;
-    float outerRadius = 1.0f;
-    float startAngle = startAngleDeg * M_PI / 180.f;
-    float end = newEndAngleInRadians;
-
-    // If end is behind start in radians, wrap it by adding 2*PI
-    if (end < startAngle) end += 2.0f * M_PI;
-
-    // add slices
-    constexpr int slices = 32;
-    // float step = (end - startAngle) / slices;
-
-    float step = (startAngle - end) / slices;
-
-    for (int i = 0; i <= slices; ++i) {
-      float theta = startAngle + i * step - (M_PI/2); 
-      mIndicatorLine.vertex(cos(theta) * innerRadius, sin(theta) * innerRadius);
-      mIndicatorLine.vertex(cos(theta) * outerRadius, sin(theta) * outerRadius);
-    }
-
-    // add color
-    for (size_t i = 0; i < mIndicatorLine.vertices().size(); ++i) {
-      mIndicatorLine.color(HSV(0.55, 0.5, 1)); // light blue
-    }
-
-    // scale
-    mIndicatorLine.scale(scale);
-  }
-
-  void printState() {
-    printf("Knob angle: %.2f degrees\n", currentEndAngleDeg);
-    printf("Knob param value: %.3f\n", currentParamValue);
-  }
-
-  void mouseEvent(Vec2f normalizedPos) {
-    Vec2f diff = normalizedPos - this->center;
-    float angle = atan2(diff[1], diff[0]) * 180.0f / M_PI;
-
-    // Normalize angle returned by atan2 into [startAngleDeg, startAngleDeg + 360)
-    if (angle < startAngleDeg) angle += 360.0f;
-
-    // Clamp to allowed range
-    angle = al::clip(angle, maxEndAngleDeg, startAngleDeg);
-    angle = startAngleDeg + (maxEndAngleDeg - angle);
-
-    this->currentEndAngleDeg = angle;
-    this->currentParamValue = al::mapRange(angle, -45.f, 225.f, 0.f, 1.f);
-    updateIndicator(this->currentEndAngleDeg * M_PI / 180.f);
-  }
-
-  void draw(Graphics& g) {
-    g.draw(mOuterCircle);
-    g.draw(mIndicatorLine);
-  }
-};
+#include "../include/knob.hpp"
 
 struct KnobApp : public al::App {
   // interface stuff
   Knob mKnob;
-
-  // audio stuff
-  giml::SinOsc<float> osc{48000}; // oscillator at 48kHz sample rate
-  bool mute = true;
   
   // graphics
   float aspectRatioScale = 1.f;
